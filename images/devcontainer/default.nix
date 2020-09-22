fb0a5ff1410f0e3ad58690db973af5824d0f9251
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-09-22 17:07:02 +1200

    Include nix support in devcontainer

--- images/devcontainer/default.nix
+++ images/devcontainer/default.nix
@@ -1,5 +1,8 @@
-# A fat and modifiable Nix image
-{ dockerTools
+{ name ? "devcontainer"
+, tag ? null
+, nixpkgsPath ? ./fake_nixpkgs
+, extraContents ? []
+, dockerTools
 , bashInteractive
 , cacert
 , closureInfo
@@ -7,7 +10,7 @@
 , curl
 , direnv
 , gcc-unwrapped
-, gitReallyMinimal
+, gitMinimal
 , glibc
 , gnugrep
 , gnused
@@ -22,15 +25,13 @@
 , procps
 , shadow
 , xz
-, mkUserEnvironment
 }:
 let
-  channel = builtins.getEnv("NIXPKGS_CHANNEL");
+  image = dockerTools.buildImageWithNixDb {
+    inherit name tag;
 
-  # generate a user profile for the image
-  profile = mkUserEnvironment {
-    derivations = [
-      # core utils
+    contents = [
+      ./root
       coreutils
       procps
       gnugrep
@@ -43,7 +44,7 @@ let
 
       # runtime dependencies of nix
       cacert
-      gitReallyMinimal
+      gitMinimal
       gnutar
       gzip
       xz
@@ -57,78 +58,41 @@ let
       # for the vscode extension
       gcc-unwrapped
       iproute
-    ];
-  };
-
-  image = dockerTools.buildImage {
-    name = "devcontainer";
-
-    contents = [ ];
+    ] ++ extraContents;
 
     extraCommands = ''
-      # create the Nix DB
-      export NIX_REMOTE=local?root=$PWD
-      export USER=nobody
-      ${nix}/bin/nix-store --load-db < ${closureInfo { rootPaths = [ profile ]; }}/registration
-
-      # set the user profile
-      ${profile}/bin/nix-env --profile nix/var/nix/profiles/default --set ${profile}
-
-      # minimal
-      mkdir -p bin usr/bin
-      ln -s /nix/var/nix/profiles/default/bin/sh bin/sh
-      ln -s /nix/var/nix/profiles/default/bin/env usr/bin/env
-
-      # might as well...
-      ln -s /nix/var/nix/profiles/default/bin/bash bin/bash
-
-      # setup shadow, bashrc
-      mkdir home
-      cp -r ${./root/etc} etc
-      chmod +w etc etc/group etc/passwd etc/shadow
-
-      # setup iana-etc for haskell binaries
-      ln -s /nix/var/nix/profiles/default/etc/protocols etc/protocols
-      ln -s /nix/var/nix/profiles/default/etc/services etc/services
+      # for /usr/bin/env
+      mkdir usr
+      ln -s ../bin usr/bin
 
       # make sure /tmp exists
-      mkdir -m 0777 tmp
+      mkdir -m 1777 tmp
+
+      # need a HOME
+      mkdir -vp root
 
       # allow ubuntu ELF binaries to run. VSCode copies it's own.
-      mkdir -p lib64
+      chmod +w lib64
       ln -s ${glibc}/lib64/ld-linux-x86-64.so.2 lib64/ld-linux-x86-64.so.2
-
-      # VSCode assumes that /sbin/ip exists
-      mkdir sbin
-      ln -s /nix/var/nix/profiles/default/bin/ip sbin/ip
+      ln -s ${gcc-unwrapped.lib}/lib64/libstdc++.so.6 lib64/libstdc++.so.6
+      chmod -w lib64
     '';
 
     config = {
-      Cmd = [ "/nix/var/nix/profiles/default/bin/bash" ];
+      Cmd = [ "/bin/bash" ];
       Env = [
-        "ENV=/nix/var/nix/profiles/default/etc/profile.d/nix.sh"
-        "GIT_SSL_CAINFO=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt"
-        "LD_LIBRARY_PATH=/nix/var/nix/profiles/default/lib"
+        "ENV=/etc/profile.d/nix.sh"
+        "GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt"
+        "LD_LIBRARY_PATH=${gcc-unwrapped.lib}/lib64"
+        "BASH_ENV=/etc/profile.d/nix.sh"
+        "NIX_BUILD_SHELL=/bin/bash"
+        "NIX_PATH=nixpkgs=${nixpkgsPath}"
         "PAGER=less"
-        "PATH=/nix/var/nix/profiles/default/bin"
-        "SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt"
-        (
-          if channel != "" then
-            "NIX_PATH=nixpkgs=channel:${channel}"
-          else
-            "NIX_PATH=nixpkgs=${../nix/fake_nixpkgs}"
-        )
+        "PATH=/usr/bin:/bin"
+        "SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt"
+        "USER=root"
       ];
-      Labels = {
-        # https://github.com/microscaling/microscaling/blob/55a2d7b91ce7513e07f8b1fd91bbed8df59aed5a/Dockerfile#L22-L33
-        "org.label-schema.vcs-ref" = "master";
-        "org.label-schema.vcs-url" = "https://github.com/nix-community/docker-nixpkgs";
-      };
     };
   };
 in
-image // {
-  meta = image.meta // {
-    description = "Nix devcontainer for VSCode";
-  };
-}
+image // { meta = nix.meta // image.meta; }
